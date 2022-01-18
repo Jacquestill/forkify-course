3174c0b891ecf782057afa4d491b887538d7b0a5
Jacquestill <jacquestill@outlook.com>
2022-01-18 20:25:04 +0900

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const PAGE_DEFAULT = 1;
+export const KEY = '1ae4bcd3-4783-4db1-9b9c-8b1dfbdd602e';
+export const MODAL_CLOSE_SEC = 2.5;
